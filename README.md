dcf8487b8ef82b0904a5f56807d765a449ac7c40
TassiloPitrasch <tassilo.pitrasch@wobeecon.com>
2025-08-18 18:01:23 +0200

    Added options to define multiple Target Paths and remove duplicate packages

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Just run the script. Make sure to allow the execution of "self-signed" scripts. 
 See the following table for command-line options:
 | Flag | Description | Default | Required |
 |--|--|--|--|
-| TargetPath | Path to the resources to be examined. Can be a single file or a directory. Supports standard Windows wildcards | - | True |
+| TargetPath | Path to the resources to be examined. Can be a single file or a directory. Supports standard Windows wildcards and allows multiple values | - | True |
 | OutputPath | File/directory the output is written to. | `packages.config` in the current working directory | False |
 | TargetFramework | Target framework as defined in [target-frameworks](https://learn.microsoft.com/en-us/nuget/reference/target-frameworks) | "" | False |
 | AllowNonDllFiles | Allows the analysis of non DLL-files, which are skipped by default. Use at own risk, might cause severe decrease of performance | False | False |
@@ -18,6 +18,7 @@ See the following table for command-line options:
 | NumericVersion | Remove non-digit components from the version. Usually increases the quality of the entries | False | False |
 | IgnoreBuild |  Ignore the build section of a version. Increases the quality of the entries | False | False |
 | IgnoreEmptyBuild | Ignore the build section if it's empty or consists only of zeros. Increases the quality of the affected entries | False | False |
+| RemoveDuplicates | Removes duplicates from the output. If disabled, a comment identifying the corresponding Target Path is added alongside the packages in `packages.config` | False | False |
 | Version | Displays the version of the script and exits afterwards | False | False |
 
 Examples for the last three options:
@@ -26,4 +27,3 @@ Examples for the last three options:
  - `1.0.0.0+b13234` becomes `1.0.0` with combined `-NumericVersion` and `-IgnoreEmpytBuild`
 
 Note that this README was written by a human and does therefore not contain a bunch of fancy emojis. If you really need them, feel free to pick one of these:  🚀 🛠️ 📦 ✅ 📄
-
--- detopa.ps1
+++ detopa.ps1
@@ -10,16 +10,15 @@ see https://github.com/TassiloPitrasch/detopa/issues
 .NOTES
 
     Author: Tassilo Pitrasch
-    Version: 0.0.3
+    Version: 0.0.4
     License: MIT
 
 #>
 
 # Parameters
 param(
-    # Path to the resources to be examined
-    [Parameter()]
-    [string]$TargetPath,
+    # Path(s) to the resources to be examined
+    [Parameter()][String[]]$TargetPaths,
 
     # Output path (directory/file)
     [Parameter()]
@@ -55,6 +54,10 @@ param(
     [Parameter()]
     [switch]$IgnoreEmptyBuild,
 
+    # Removes duplicate entries from the output file
+    [Parameter()]
+    [switch]$RemoveDuplicates,
+
     # Output version info
     [Parameter()]
     [switch]$Version
@@ -88,6 +91,86 @@ function Cleanse-Version {
 }
 
 
+# Function to examine a target (path) - main method of the script
+function Examine-Target-Path {
+    param(
+        [Parameter(Mandatory)]
+        [string]$TargetPath
+    )
+
+    # If duplicates are not to be removed from the output file,
+    # a comment is added identifying each target
+    if (-not $RemoveDuplicates) {
+        $xmlWriter.WriteComment($TargetPath)
+    }
+
+    # Retrieve the target files
+    $files = Get-ChildItem -Path $TargetPath
+
+    # Process files one-by-one
+    foreach ($file in $files) {
+        # Skip non-DLLs unless explicitly allowed
+        if (-not $AllowNonDllFiles -and -not $file.Extension.Equals('.dll', 'InvariantCultureIgnoreCase')) {
+            Write-Debug ("Skipping non-DLL file '{0}'." -f $file.Name)
+            continue
+        }
+
+        Write-Debug ("Examining file '{0}'." -f $file.Name)
+
+        # Extract file metadata
+        $info = Get-Item -Path $file.FullName
+        $versionInfo = $info.VersionInfo
+
+        # Clean the name fields by removing the ".dll" extension
+        $internalName = $versionInfo.InternalName -replace '\.dll$', ''
+        $originalFileName = $versionInfo.OriginalFilename -replace '\.dll$', ''
+
+        # Determine package name
+        $packageName = if ($internalName) {
+            Write-Debug "Using InternalName field."
+            $internalName
+        } elseif ($originalFileName) {
+            Write-Debug "Using OriginalFilename field."
+            $originalFileName
+        } elseif ($UseBasename) {
+            Write-Debug "Using file basename."
+            $info.BaseName
+        } else {
+            Write-Debug ("Skipping '{0}' — no package name found." -f $file.Name)
+            continue
+        }
+
+        # Parse and cleanse package version
+        $packageVersion = $versionInfo.ProductVersion
+        if ([string]::IsNullOrEmpty($packageVersion)) {
+            if (-not $AllowEmptyVersions) {
+                Write-Debug ("Skipping '{0}' — no version info available." -f $file.Name)
+                continue
+            }
+            Write-Debug ("Writing package '{0}' without version." -f $packageName)
+            $finalPackageVersion = ''
+        } else {
+            $finalPackageVersion = Cleanse-Version -RawVersion $packageVersion
+        }
+
+        # Skipping duplicates (if enabled)
+        $packageIdentifier = "{0}/{1}" -f $packageName, $finalPackageVersion
+        if ($examinedPackages[$packageIdentifier]) {
+            continue
+        } else {
+            $examinedPackages[$packageIdentifier] = $RemoveDuplicates
+        }
+
+        # Write package entry to XML
+        Write-Debug ("Writing package '{0}/{1}' to output." -f $packageName, $finalPackageVersion)
+        $xmlWriter.WriteStartElement('package')
+        $xmlWriter.WriteAttributeString('id', $packageName)
+        $xmlWriter.WriteAttributeString('version', $finalPackageVersion)
+        $xmlWriter.WriteAttributeString('targetFramework', $TargetFramework)
+        $xmlWriter.WriteEndElement()
+    }
+}
+
 # Enable debug output without script halting
 if ($PSBoundParameters.ContainsKey('Debug')) {
     $DebugPreference = 'Continue'
@@ -106,9 +189,6 @@ if ($Version) {
 if ([string]::IsNullOrEmpty($TargetPath)) {
     throw [System.ArgumentException] "Parameter 'TargetPath' is required."
 }
-
-# Retrieve the target files
-$files = Get-ChildItem -Path $TargetPath
 
 # Create a temporary file path for XML output
 $temporaryOutputPath = New-TemporaryFile
@@ -135,59 +215,14 @@ $xmlWriter.IndentChar = " "
 $xmlWriter.WriteStartDocument()
 $xmlWriter.WriteStartElement('packages')
 
-# Process files one-by-one
-foreach ($file in $files) {
-    # Skip non-DLLs unless explicitly allowed
-    if (-not $AllowNonDllFiles -and -not $file.Extension.Equals('.dll', 'InvariantCultureIgnoreCase')) {
-        Write-Debug ("Skipping non-DLL file '{0}'." -f $file.Name)
-        continue
-    }
+# Hashtable containing the examined packages
+$examinedPackages = @{}
 
-    Write-Debug ("Examining file '{0}'." -f $file.Name)
 
-    # Extract file metadata
-    $info = Get-Item -Path $file.FullName
-    $versionInfo = $info.VersionInfo
-
-    # Clean the name fields by removing the ".dll" extension
-    $internalName = $versionInfo.InternalName -replace '\.dll$', ''
-    $originalFileName = $versionInfo.OriginalFilename -replace '\.dll$', ''
-
-    # Determine package name
-    $packageName = if ($internalName) {
-        Write-Debug "Using InternalName field."
-        $internalName
-    } elseif ($originalFileName) {
-        Write-Debug "Using OriginalFilename field."
-        $originalFileName
-    } elseif ($UseBasename) {
-        Write-Debug "Using file basename."
-        $info.BaseName
-    } else {
-        Write-Debug ("Skipping '{0}' — no package name found." -f $file.Name)
-        continue
-    }
-
-    # Parse and cleanse package version
-    $packageVersion = $versionInfo.ProductVersion
-    if ([string]::IsNullOrEmpty($packageVersion)) {
-        if (-not $AllowEmptyVersions) {
-            Write-Debug ("Skipping '{0}' — no version info available." -f $file.Name)
-            continue
-        }
-        Write-Debug ("Writing package '{0}' without version." -f $packageName)
-        $finalPackageVersion = ''
-    } else {
-        $finalPackageVersion = Cleanse-Version -RawVersion $packageVersion
-    }
-
-    # Write package entry to XML
-    Write-Debug ("Writing package '{0}/{1}' to output." -f $packageName, $finalPackageVersion)
-    $xmlWriter.WriteStartElement('package')
-    $xmlWriter.WriteAttributeString('id', $packageName)
-    $xmlWriter.WriteAttributeString('version', $finalPackageVersion)
-    $xmlWriter.WriteAttributeString('targetFramework', $TargetFramework)
-    $xmlWriter.WriteEndElement()
+# Examine each target path
+foreach ($TargetPath in $TargetPaths)  {
+    Write-Debug ("Examining path '{0}'." -f $TargetPath)
+    Examine-Target-Path $TargetPath
 }
 
 # Finalize the XML document
